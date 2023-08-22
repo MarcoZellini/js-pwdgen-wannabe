e90fc38c9b05bcf2a01b5407197ad26a5b7823dc
Marco Zellini <zellinimarco@gmail.com>
2023-08-22 13:58:27 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*
+    Descrizione problema:
+    Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+    che va formattato in questo modo: nomecognomecolorepreferito23 
+    Esempio: fabiopacificiblue23
+
+*/
+
+
+
