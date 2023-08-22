97be6bbdebe1465e3ec57609bfe8fdc17906c67d
Marco Zellini <zellinimarco@gmail.com>
2023-08-22 13:59:39 +0200

    Add pwd Generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,31 @@
     che va formattato in questo modo: nomecognomecolorepreferito23 
     Esempio: fabiopacificiblue23
 
+    Strumenti:
+        prompt
+        const
+        getElementById
+        console.log
+
+    Pseudo Codice:
+        - Richiedo all'utente il suo nome
+        - Richiedo all'utente il suo cognome
+        - Richiedo all'utente il suo colore preferito
+        - Concateno le stringhe aggiungendo '23' alla fine
+        - Scrivo in console la password generata
+        - Scrivo in pagina la password generata
 */
+
+const userName = prompt("Inserisci il tuo nome: ");
+const userSurname = prompt("Inserisci il tuo cognome: ");
+const userFavoriteColor = prompt("Inserisci il tuo colore preferito: ");
+const passwordElement = document.getElementById('password');
+
+const password = `${userName}${userSurname}${userFavoriteColor}23`
+
+console.log(password);
+passwordElement.innerHTML = `La tua nuova password è: <strong>${password}</strong>`;
+
 
 
 
